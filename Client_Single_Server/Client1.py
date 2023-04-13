d1a607ce5b07e7306c67cbd5ae372256406b02bf
junedeldeleon <junedel_deleon@dlsu.edu.ph>
2023-04-13 09:05:01 +0800

    Updated clients

--- Client_Single_Server/Client1.py
+++ Client_Single_Server/Client1.py
@@ -1,36 +1,31 @@
-#This code is from ACTIVITY 2
-
-import pika, random, json
+import pika, random, json, time
 credentials = pika.PlainCredentials('rabbituser', 'rabbit1234')
 connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.5.39', 5672 ,'/', credentials))
 channel = connection.channel()
 channel.queue_declare(queue='singlequeue') 
 
-#While True:
-#DATA GENERATION
-heartRate = random.randint(50, 110)
-longitude = random.uniform(-180.0, 180.0)
-latitude = random.uniform(-90.0, 90.0)
-steps = random.randint(100, 10000)
-distance = random.randint(100, 10000)
-cal = random.randint(500, 1500)
-temperature = random.uniform(34.0, 40.0)
 while True:
-    bloodPNum = random.randint(80, 150)
-    bloodPDen = random.randint(60, 100)
-    if bloodPNum < bloodPDen:
+    try:
+        #DATA GENERATION
+        heartRate = random.randint(50, 110)
+        longitude = random.uniform(-180.0, 180.0)
+        latitude = random.uniform(-90.0, 90.0)
+        steps = random.randint(100, 10000)
+        distance = random.randint(100, 10000)
+        cal = random.randint(500, 1500)
+        temperature = random.uniform(34.0, 40.0)
+
+        # msg = "{{\"AppID\":\"1\",\"steps\":\"{}\",\"heart rate\":\"{}\"}}".format(steps, heartRate)
+        # print(msg)
+        msg_dict={'AppID':2, 'name':'Ben', 'steps':steps, 'calories':cal}
+        print (msg_dict)
+        msg_json=json.dumps(msg_dict) 
+        channel.basic_publish(exchange='', routing_key='singlequeue', body=msg_json) 
+        #Specify routing key so we can route which server application takes the data
+        connection.process_data_events() # check for any incoming or outgoing messages
+        time.sleep(3)
+    except pika.exceptions.ConnectionClosed:
+        print("Connection closed. Closing program...")
         break
 
-# msg = "{{\"AppID\":\"1\",\"steps\":\"{}\",\"heart rate\":\"{}\"}}".format(steps, heartRate)
-# print(msg)
-
-# name=input('Enter your name: ')
-# msg=input('Enter your message: ') 
-msg_dict={'AppID':2, 'name':'Ben', 'steps':steps, 'calories':cal}
-print (msg_dict)
-msg_json=json.dumps(msg_dict) 
-channel.basic_publish(exchange='', routing_key='singlequeue', body=msg_json) 
-#Specify routing key so we can route which server application takes the data
-
-print(" [x] Sent '%s'",(msg_dict))
 connection.close()
